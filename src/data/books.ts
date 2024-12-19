2240911daeaeb157b8e3b5a65e8a1b446deaf9ff
geraldy1st <geraldy.leondas@gmail.com>
2024-12-19 11:39:02 +0000

    images fixed

--- src/data/books.ts
+++ src/data/books.ts
@@ -3,20 +3,21 @@ import { Book } from "../types/Book";
 export const books: Book[] = [
   {
     id: "1",
-    title: "50 Questions Réponses sur la Croyance",
-    description:
-      "Un livre essentiel pour comprendre les fondements de la croyance islamique à travers 50 questions-réponses détaillées.",
-    price: 19.99,
-    imageUrl: "/croyance.jpeg",
-    priceId: "price_H5ggYwtDq9",
-  },
-  {
-    id: "2",
     title: "La Voie du Petit Musulman",
     description:
       "Un guide précieux pour l'apprentissage des hadiths du Prophète, adapté aux jeunes musulmans.",
     price: 19.99,
-    imageUrl: "/la_voie_du_petit_musulman.jpeg",
+    imageUrl: "../../public/images/la_voie_du_petit_musulman.jpeg",
     priceId: "price_H5ggYwtDq8",
+  },
+
+  {
+    id: "2",
+    title: "50 Questions Réponses sur la Croyance",
+    description:
+      "Un livre essentiel pour comprendre les fondements de la croyance islamique à travers 50 questions-réponses détaillées.",
+    price: 19.99,
+    imageUrl: "../../public/images/croyance.jpeg",
+    priceId: "price_H5ggYwtDq9",
   },
 ];
